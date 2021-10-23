d8ceb6c05ca3aa0c2bf48feded93e5c54263a2ad
Hikmet Altıntaş <hikmet1517@gmail.com>
2021-10-23 22:08:49 +0300

    fix icon, update uninstall

--- uninstall_plasmoid.sh
+++ uninstall_plasmoid.sh
@@ -3,5 +3,6 @@
 sudo rm -rf /usr/lib/qt/qml/org/kde/private/archUpdate/
 sudo rm -rf /usr/share/plasma/plasmoids/org.kde.archUpdate/
 sudo rm /usr/share/metainfo/org.kde.archUpdate.appdata.xml
+sudo rm /usr/share/kservices5/plasma-applet-org.kde.archUpdate.desktop
 
 killall plasmashell && kstart5 plasmashell
